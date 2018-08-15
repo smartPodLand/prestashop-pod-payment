ecf6f90f14cbe497c31987029fd6c9bee4cd41ed
Mehran Rahbardar <rahbardar.mehran@gmail.com>
2018-08-15 16:49:25 +0430

    limit to only pod users  option added

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 This module requires [pod sso](https://github.com/smartPodLand/prestashop-pod-sso) module installed and configured.
 All of tokens and urls must be configured in pod sso plugin read more in its [readme](https://github.com/smartPodLand/prestashop-pod-sso/blob/master/README.md)
 
+## Installation
+If you download the module from github, first unzip it then rename `prestashop-pod-payment-master` to `podpay` and compress it as a zip format. Then use prestashop module installer for uploading the module to your server. 
--- podpay.php
+++ podpay.php
@@ -2,6 +2,12 @@
 if(!defined('_PS_VERSION_')){
     exit;
 }
+if (!defined('ALL')){
+	define('ALL', 4);
+}
+if(!defined('ONLY')){
+	define('ONLY', 2);
+}
 class PodPay extends PaymentModule {
 
 	private $_html = '';
@@ -11,7 +17,7 @@ class PodPay extends PaymentModule {
 
 		$this->name = 'podpay';
 		$this->tab = 'payments_gateways';
-		$this->version = '1.0.4';
+		$this->version = '1.1.0';
 		$this->author = 'Mehran Rahbardar';
 		$this->currencies = true;
 		$this->currencies_mode = 'radio';
@@ -19,7 +25,7 @@ class PodPay extends PaymentModule {
         $this->ps_versions_compliancy = array('min' => '1.5', 'max' => '1.6');
         $this->bootstrap = true;
 		parent::__construct();
-		$this->displayName = $this->l('Pod Payment Modlue');
+		$this->displayName = $this->l('Pod Payment Module');
 		$this->description = $this->l('Online Payment With Pod');
 		$this->confirmUninstall = $this->l('Are you sure you want to delete your details?');
 
@@ -39,8 +45,21 @@ class PodPay extends PaymentModule {
 			return true;
 	}
 public function hookPayment($params) {
-    if ($this->active)
-        return $this->display(__FILE__, 'podpayment.tpl');
+		$pod_only = Configuration::get('POD_ONLY');
+
+		$login_by_pod = $this->context->cookie->login_by_pod;
+
+	if ($this->active){
+		if($pod_only==ONLY){
+			if($login_by_pod){
+				return $this->display(__FILE__, 'podpayment.tpl');
+			}
+		}
+		else if ($pod_only==ALL) {
+			return $this->display(__FILE__, 'podpayment.tpl');
+		}
+
+	}
 }
 
 public function hookPaymentReturn($params) {
